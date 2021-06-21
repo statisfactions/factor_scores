b3bff97d0a6064650d84404a6ae1a6d216097ac3
Ethan Brown <brow3821@umn.edu>
2021-06-21 10:41:50 -0700

    update efa code

--- 2021-06-15 fifth_simulation_efa.R
+++ 2021-06-15 fifth_simulation_efa.R
@@ -8,25 +8,56 @@ library(future)
 library(tictoc)
 library(testthat)
 library(glue)
-library(parallel)
 library(semTools)
 source("sim_fns.R")
 
 ## Specify ----
-nfactor_data = list.files("nfactor_results", pattern = "RDS$", full.names = TRUE) %>% 
+
+get_nfactor_data = function(done_files) {
+
+nfactor_data = list.files("nfactor_results", pattern = "RDS$", full.names = TRUE) %>%
+setdiff(done_files) %>%
   purrr::set_names(.)
 
-nfactor_results = map_dfr(nfactor_data, readRDS, .id = "filename") %>% rename(n_factors = sim_cell) 
+nfactor_results = map_dfr(nfactor_data, readRDS, .id = "filename") %>% rename(n_factors = sim_cell) %>% 
+  mutate(rep = filename %>% 
+           str_remove(".RDS$") %>% str_extract("[0-9]+$") %>% as.integer)            
+}
 
-#nfactor_results
+if(file.exists("nfactor_completed.RDS")) {
+nfactor_completed = readRDS("nfactor_completed.RDS")
+} else {
+	nfactor_completed = c()
+}
+
 
 ## Fit efas -----
+nfiles = 20 # number of EFA files to do simultaneously
 plan(multicore)
-tic()
-gen_efa = nfactor_results %>% reproduce(colname = "sim_cell", fn = fit_and_tidy_efa_only, 
-                                      globals = TRUE, packages = "GPArotation")
-toc() #753 seconds
-saveRDS(gen_efa, file = "efas_1.RDS")
+runtime = Sys.time() %>% str_replace_all(":", "_")
+for(j in 1:10000) {
+
+	system("git pull")
+nfactor_results = get_nfactor_data(nfactor_completed)
+
+batch_files = sort(unique(nfactor_results$filename))[1:nfiles] 
+if(any(str_detect(batch_files, "all_conditions")))
+	batch_files = batch_files[1]
+	
+batch = nfactor_results %>% filter(filename %in% batch_files) 
+
+batch_name = with(batch[1,], paste(r, factor_condition, rep))
+cat("Batch ", batch_files, "\n")
+
+
+  tic()
+  gen_efa = batch %>%  reproduce(colname = "sim_cell", fn = fit_and_tidy_efa_only, 
+                                                               globals = TRUE, packages = "GPArotation")
+  toc() #753 seconds
+  nfactor_completed = c(nfactor_completed, gen_efa$filename)
+  saveRDS(nfactor_completed, file = "nfactor_completed.RDS")
+  saveRDS(gen_efa, file = glue("efa_results/{runtime} {batch_name} efas {length(batch_files)} files.RDS"))
+}
 
 
 # tic()
